d4705d920c03e22aaab6b4c855604b503fa453e4
Randikaviraj <56146699+Randikaviraj@users.noreply.github.com>
2023-01-06 22:01:39 +0530

    Custom instruction added to Nios

---
+++ CUSTOM_INSTRUCTION_ADDED_NIOS_II_CRC_SOC/software/CRC_CUSTOM/obj/default/ci_crc.d
@@ -0,0 +1,6 @@
+obj/default/ci_crc.o: ci_crc.c ../CRC_CUSTOM_bsp/system.h \
+  ../CRC_CUSTOM_bsp/linker.h
+
+../CRC_CUSTOM_bsp/system.h:
+
+../CRC_CUSTOM_bsp/linker.h:
---
+++ CUSTOM_INSTRUCTION_ADDED_NIOS_II_CRC_SOC/software/CRC_CUSTOM/obj/default/crc_main.d
@@ -0,0 +1,42 @@
+obj/default/crc_main.o: crc_main.c ../CRC_CUSTOM_bsp/system.h \
+  ../CRC_CUSTOM_bsp/linker.h crc.h ci_crc.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/sys/alt_timestamp.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/alt_types.h \
+  ../CRC_CUSTOM_bsp//drivers/inc/altera_avalon_timer.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/alt_types.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/sys/alt_dev.h ../CRC_CUSTOM_bsp/system.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/sys/alt_llist.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/sys/alt_llist.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/alt_types.h \
+  ../CRC_CUSTOM_bsp//HAL/inc/sys/alt_warning.h
+
+../CRC_CUSTOM_bsp/system.h:
+
+../CRC_CUSTOM_bsp/linker.h:
+
+crc.h:
+
+ci_crc.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/alt_types.h:
+
+../CRC_CUSTOM_bsp//drivers/inc/altera_avalon_timer.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/alt_types.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/sys/alt_dev.h:
+
+../CRC_CUSTOM_bsp/system.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/sys/alt_llist.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/sys/alt_llist.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/alt_types.h:
+
+../CRC_CUSTOM_bsp//HAL/inc/sys/alt_warning.h:
